d311ac9f4653ec13dc065941b3a5e36245a2022a
hesam <hesvmpiri@gmail.com>
2025-07-18 00:54:10 +0330

    add functionality to login

--- components/loginDialog.tsx
+++ components/loginDialog.tsx
@@ -1,92 +1,134 @@
-'use client'
-import React, { useState } from 'react'
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
-import { z } from 'zod' 
+"use client";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { z } from "zod";
 import { useForm } from "react-hook-form";
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Eye, EyeOff } from 'lucide-react';
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Eye, EyeOff, Loader2 } from "lucide-react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const formSchema = z.object({
-  username: z.string().min(4, {
-    message: "Username must be at least 4 characters.",
-  }),
+  email: z.string().email(),
   password: z.string().min(8, {
     message: "Password must be at least 8 characters.",
   }),
-})
+});
 
 const LoginDialog = () => {
-
-  const [showPassword, setShowPassword] = useState(false)
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+  const [open, setopen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      email: "",
       password: "",
     },
-  })
+  });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true);
+    const res = await signIn("credentials", {
+      redirect: false,
+      ...values,
+    });
+
+    setIsLoading(false)
+    
+    if (res?.error) {
+      toast.error("invalid" + res.error);
+    } else {
+      setopen(false);
+      router.push("/dashboard");
+      form.reset();
+    }
   }
   return (
-    <Dialog>
-    <DialogTrigger asChild>
-      <Button variant="default">Login </Button>
-    </DialogTrigger>
-    <DialogContent className="sm:max-w-[425px]">
-      <DialogHeader>
-        <DialogTitle>Login</DialogTitle>
-        <DialogDescription>
-          please enter your details
-        </DialogDescription>
-      </DialogHeader>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-            <FormField control={form.control} name="username" render={({field})=>(
+    <Dialog open={open} onOpenChange={setopen}>
+      <DialogTrigger asChild>
+        <Button variant="default">Login </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        {isLoading && (
+          <div className="absolute inset-0 flex items-center justify-center bg-black/50">
+            <Loader2 className="w-10 h-10 animate-spin" />
+          </div>
+        )}
+        <DialogHeader>
+          <DialogTitle>Login</DialogTitle>
+          <DialogDescription>please enter your details</DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
                 <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                        <Input {...field} />
-                    </FormControl>
-                    <FormMessage/>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
                 </FormItem>
-            )}/>
-            <FormField control={form.control} name="password" render={({field})=>(
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
                 <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <div className='relative'>
-                        <Input {...field} type={showPassword ? "text" : "password"} />
-                        <Button type='button' variant='ghost' className='absolute right-0 top-0' onClick={()=>setShowPassword(!showPassword)}>
-                          {showPassword ? <Eye /> : <EyeOff />}
-                        </Button>
-                      </div>
-                    </FormControl>
-                    <FormMessage/>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Input
+                        {...field}
+                        type={showPassword ? "text" : "password"}
+                      />
+                      <Button
+                        type="button"
+                        variant="ghost"
+                        className="absolute right-0 top-0"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? <Eye /> : <EyeOff />}
+                      </Button>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
                 </FormItem>
-            )}/>
+              )}
+            />
             <DialogFooter>
               <Button type="submit">Login</Button>
             </DialogFooter>
-        </form>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-      </Form>
-    </DialogContent>
-  </Dialog>
-  )
-}
-
-export default LoginDialog
+export default LoginDialog;
