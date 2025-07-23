fa0e9137726c7eb293e4431d0268d63ca9fe7d7d
hesam <hesvmpiri@gmail.com>
2025-07-23 23:58:08 +0330

    add a filtering nav for mobile screen

--- app/job-list/page.tsx
+++ app/job-list/page.tsx
@@ -1,11 +1,24 @@
+import FilterBar from "@/components/filterBar";
 import JobList from "@/components/jobList";
+import SortBySelect from "@/components/sortBySelect";
 
-const JobListPage = () => {
-
+const JobListPage = ({
+  searchParams,
+}: {
+  searchParams?: {
+    type?: string;
+    location?: string;
+    category?: string;
+  };
+}) => {
   return (
     <div className="container mx-auto">
-      <h1>Job List</h1>
-      <JobList />
+      <div className="flex sm:py-5 p-2  items-center">
+        <SortBySelect/>
+        {/* <p className="text-sm text-gray-500">suggestions</p> */}
+        <FilterBar />
+      </div>
+      <JobList searchParams={searchParams || {}} />
     </div>
   );
 };
---
+++ components/filterBar.tsx
@@ -0,0 +1,28 @@
+"use client"
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { Menu, SlidersHorizontal, X } from 'lucide-react'
+import Filterform from '@/froms/filterform'
+import { Button } from './ui/button'
+
+const FilterBar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div className='sm:hidden ml-auto'>
+        <Button onClick={() => setIsOpen(!isOpen)} className="p-" variant={"outline"} size={'sm'}>
+            <SlidersHorizontal size={15} />
+        </Button>
+        <nav className={`fixed top-0 right-0 h-screen transition-all duration-200 ${isOpen ? "w-full" : "w-0"} overflow-hidden z-10 bg-black text-white `}>
+            <div className="p-4 flex justify-between items-center">
+                <h2 className="text-xl font-bold">Filters</h2>
+                <button onClick={() => setIsOpen(false)} className="p-2">
+                    <X className="w-6 h-6" />
+                </button>
+            </div>
+            <Filterform/>
+        </nav>
+    </div>
+  )
+}
+
+export default FilterBar
