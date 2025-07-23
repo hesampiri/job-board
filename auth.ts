b3c895fb7e290b22a857ae89392c81628175bb8a
hesam <hesvmpiri@gmail.com>
2025-07-23 23:51:31 +0330

    added id to session

--- auth.ts
+++ auth.ts
@@ -2,20 +2,29 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { prisma } from "./prisma";
 import bcrypt from "bcryptjs";
+import { DefaultJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface User {
+    id: string;
     role: "employer" | "jobseeker";
-    companyId: string | null
+    companyId: string | null;
   }
   interface Session {
-    user:{
+    user: {
+      id: string;
       role: "employer" | "jobseeker";
-      companyId: string | null
-    }
+      companyId: string | null;
+    };
   }
 }
-
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    id: string;
+    role: "employer" | "jobseeker";
+    companyId: string | null;
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -38,7 +47,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         });
         if (!user) return null;
         const passwordsmatch = await bcrypt.compare(password, user.password);
-        if (passwordsmatch) return user;
+        if (passwordsmatch)
+          return {
+            id: user.id,
+            email: user.email,
+            role: user.role,
+            companyId: user.companyId,
+          };
         return null;
       },
     }),
@@ -47,17 +62,19 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     strategy: "jwt",
   },
   callbacks: {
-    async jwt({user , token}){
-      if(user){
-        token.role = user.role
-        token.companyId = user.companyId
+    async jwt({ user, token }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+        token.companyId = user.companyId;
       }
-      return token
+      return token;
     },
-    async session({token , session}){
-      session.user.role = token.role as "employer" | "jobseeker"
-      session.user.companyId = token.companyId as string
-      return session
-    }
+    async session({ token, session }) {
+      session.user.id = token.id as string;
+      session.user.role = token.role as "employer" | "jobseeker";
+      session.user.companyId = token.companyId as string;
+      return session;
+    },
   },
 });
