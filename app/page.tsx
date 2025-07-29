5ec032e1ead99caed808e75b24838b3ec91b9361
hesam <hesvmpiri@gmail.com>
2025-07-30 01:18:43 +0330

    change design

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,28 @@
-"use client"
+"use client";
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
   return (
-    <main className="min-h-screen p-4 flex flex-col items-center justify-center">
-      <h1 className="text-2xl sm:text-5xl font-bold mb-4">looking for job or wants to hire somebody ?</h1>
-      <div className="flex flex-col sm:flex-row gap-4 justify-center items-center w-full max-w-7xl mx-auto mt-10">
-        <div className="p-4 sm:p-5 rounded-lg text-center w-full sm:w-[400px] cursor-pointer hover:shadow-lg hover:bg-gray-200 border-2 border-black transition-shadow duration-300 bg-white/5" onClick={() => router.push('/job-list')}>
-          <h1 className="text-lg sm:text-xl font-bold capitalize">job seeker</h1>
-          <p className="text-xs sm:text-sm text-gray-500">looking for a job</p>
-        </div>
-        <div className="p-4 sm:p-5 rounded-lg text-center w-full sm:w-[400px] cursor-pointer hover:shadow-lg hover:bg-gray-200 border-2 border-black transition-shadow duration-300 bg-white/5" onClick={() => router.push('/job-list')}>
-          <h1 className="text-lg sm:text-xl font-bold capitalize">employer</h1>
-          <p className="text-xs sm:text-sm text-gray-500">looking for a employee</p>
+    <main className="min-h-screen p-4 flex justify-center">
+      <div className="flex flex-col gap-5 items-center py-24">
+        <h1 className="capitalize font-bold text-6xl">
+          find your dream <span className="text-yellow-400">job</span>
+        </h1>
+        <p className="text-gray-500">
+          Discover top jobs, connect with leading companies, and take the next
+          step in your career. Whether you're hiring or job hunting — we make it
+          simple, fast, and effective.
+        </p>
+        <div className="flex gap-2">
+          <Button>Explore Jobs</Button>
+          <Button
+            className="text-yellow-400 border-yellow-400"
+            variant={"outline"}
+          >
+            Signup
+          </Button>
         </div>
       </div>
     </main>
